40263b431c82e42bccaa7f8a8b43c189b20f0e46
avredeveld <agevredeveld@hotmail.com>
2016-12-19 13:09:57 +0100

    main object

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a, b, c, d;
+
+        a = 6;
+        b = 7;
+        c = 8;
+        d = 9;
+
+        if(a < b){
+            System.out.println("Hallo");
+
+        }
+    }
+}
